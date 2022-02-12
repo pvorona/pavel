e0eaf33599a03c2a8ca08d3f3869cfb3bce5d4b3
pavel <vorona.pavel.nickolaevich@gmail.com>
2022-02-12 11:40:01 +0300

    Extract function

--- packages/scheduling/src/lib/scheduleTask/scheduleTask.ts
+++ packages/scheduling/src/lib/scheduleTask/scheduleTask.ts
@@ -1,8 +1,8 @@
-import { futureQueueByPriority } from '..'
 import { PRIORITY, PHASE } from '../constants'
 import {
   queueByPriority,
   scheduleExecutionIfNeeded,
+  futureQueueByPriority,
   phase,
 } from '../scheduling'
 import { Lambda } from '../types'
@@ -11,7 +11,7 @@ export function scheduleTask(task: Lambda, priority = PRIORITY.WRITE): Lambda {
   // Capture queue of the current frame
   // to prevent modifications of the future queues
   // when cancelling this task
-  const { isCancelledByIndex, tasks } = getQueue()[priority]
+  const { isCancelledByIndex, tasks } = getQueue(priority)[priority]
   const nextIndex = tasks.push(task)
 
   scheduleExecutionIfNeeded()
@@ -19,12 +19,12 @@ export function scheduleTask(task: Lambda, priority = PRIORITY.WRITE): Lambda {
   return function cancelTask() {
     isCancelledByIndex[nextIndex - 1] = true
   }
+}
 
-  function getQueue() {
-    if (priority === PRIORITY.BEFORE_RENDER && phase === PHASE.RENDERING) {
-      return futureQueueByPriority
-    }
-
-    return queueByPriority
+function getQueue(priority: PRIORITY) {
+  if (priority === PRIORITY.BEFORE_RENDER && phase === PHASE.RENDERING) {
+    return futureQueueByPriority
   }
+
+  return queueByPriority
 }
