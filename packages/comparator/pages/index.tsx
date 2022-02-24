e93a7dd97cc59faf2c3ee5e75f057471e00989df
Pavel Vorona <vorona.pavel.nickolaevich@gmail.com>
2022-02-24 17:06:04 +0300

    Comparator (#19)

---
+++ packages/comparator/pages/index.tsx
@@ -0,0 +1,9 @@
+export function Index() {
+  return (
+    <div className="mx-auto font-bold text-5xl h-screen flex justify-center items-center text-teal-500">
+      <div>Hello friends!</div>
+    </div>
+  )
+}
+
+export default Index
