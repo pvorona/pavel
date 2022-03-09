ffc2d9942c8088beaeb55ed6ecfc1fbea456aa62
pavel <vorona.pavel.nickolaevich@gmail.com>
2022-03-09 22:36:58 +0300

    Document size

--- packages/comparator/pages/_document.tsx
+++ packages/comparator/pages/_document.tsx
@@ -2,9 +2,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="en" className="h-full">
       <Head />
-      <body className="dark:bg-gray-6 dark:text-[#e7eaed] selection:bg-black selection:text-white dark:selection:bg-gray-3">
+      <body className="h-full dark:bg-gray-6 dark:text-[#e7eaed] selection:bg-black selection:text-white dark:selection:bg-gray-3">
         <Main />
         <NextScript />
       </body>
