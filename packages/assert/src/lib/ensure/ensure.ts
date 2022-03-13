bc7fe3f6f8b4d2c69c42e9b8c69d2de7b82650b1
pavel <vorona.pavel.nickolaevich@gmail.com>
2022-03-14 01:51:14 +0300

    Improve debuggability

--- packages/assert/src/lib/ensure/ensure.ts
+++ packages/assert/src/lib/ensure/ensure.ts
@@ -1,9 +1,13 @@
 export const ensure =
   <T>(predicate: (value: unknown) => boolean) =>
-  (value: unknown): T => {
+  (value: unknown, identifier?: string): T => {
     if (predicate(value)) {
       return value as T
     }
 
-    throw new Error(`Expected ${value} to satisfy predicate`)
+    throw new Error(
+      `Expected ${value} to satisfy predicate${
+        identifier ? ` when evaluating ${identifier}` : ''
+      }`,
+    )
   }
--- packages/comparator-shared/src/lib/firebase/initAuth.ts
+++ packages/comparator-shared/src/lib/firebase/initAuth.ts
@@ -18,29 +18,49 @@ export const initAuth = () => {
     },
     firebaseAdminInitConfig: {
       credential: {
-        projectId: ensureString(process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID),
+        projectId: ensureString(
+          process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+          'NEXT_PUBLIC_FIREBASE_PROJECT_ID',
+        ),
         clientEmail: process.env.FIREBASE_CLIENT_EMAIL
-          ? ensureString(process.env.FIREBASE_CLIENT_EMAIL)
+          ? ensureString(
+              process.env.FIREBASE_CLIENT_EMAIL,
+              'FIREBASE_CLIENT_EMAIL',
+            )
           : undefined,
         // The private key must not be accessible on the client
         privateKey: process.env.FIREBASE_PRIVATE_KEY
-          ? JSON.parse(process.env.FIREBASE_PRIVATE_KEY)
+          ? process.env.FIREBASE_PRIVATE_KEY
           : undefined,
       },
     },
     // Use application default credentials (takes precedence over fireaseAdminInitConfig if set)
     // useFirebaseAdminDefaultCredential: true,
     firebaseClientInitConfig: {
-      apiKey: ensureString(process.env.NEXT_PUBLIC_FIREBASE_API_KEY),
-      authDomain: ensureString(process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN),
-      projectId: ensureString(process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID),
+      apiKey: ensureString(
+        process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+        'NEXT_PUBLIC_FIREBASE_API_KEY',
+      ),
+      authDomain: ensureString(
+        process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+        'NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN',
+      ),
+      projectId: ensureString(
+        process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+        'NEXT_PUBLIC_FIREBASE_PROJECT_ID',
+      ),
       storageBucket: ensureString(
         process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+        'NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET',
       ),
       messagingSenderId: ensureString(
         process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+        'NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID',
       ),
-      appId: ensureString(process.env.NEXT_PUBLIC_FIREBASE_APP_ID),
+      appId: ensureString(
+        process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+        'NEXT_PUBLIC_FIREBASE_APP_ID',
+      ),
     },
     cookies: {
       name: 'Comparator',
