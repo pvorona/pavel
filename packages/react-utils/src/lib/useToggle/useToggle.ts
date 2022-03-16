33ea95642efa478d4496c024089aa12886227637
pavel <vorona.pavel.nickolaevich@gmail.com>
2022-03-16 12:38:34 +0300

    Add `useToggle`

---
+++ packages/react-utils/src/lib/useToggle/useToggle.ts
@@ -0,0 +1,15 @@
+import { isUndefined } from 'lodash'
+import { useState, useCallback } from 'react'
+
+export default function useToggle(initialState = false) {
+  const [state, setState] = useState(initialState)
+
+  const toggle = useCallback((maybeValue?: boolean) => {
+    if (isUndefined(maybeValue)) {
+      setState(value => !value)
+    } else {
+      setState(maybeValue)
+    }
+  }, [])
+  return [state, toggle]
+}
