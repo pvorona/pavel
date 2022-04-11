29bf3c501f13b2ab4a9f68b3027fa106e08ffd69
pavel <vorona.pavel.nickolaevich@gmail.com>
2022-04-11 21:57:22 +0300

    Hide some planets in dark scheme

--- packages/comparator/next.config.js
+++ packages/comparator/next.config.js
@@ -10,6 +10,20 @@ const nextConfig = {
     // See: https://github.com/gregberge/svgr
     svgr: false,
   },
+  async headers() {
+    return [
+      {
+        source: '/:all*(svg|jpg|png|woff2)',
+        locale: false,
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=9999999999, must-revalidate',
+          },
+        ],
+      },
+    ]
+  },
 }
 
 module.exports = withNx(nextConfig)
--- packages/components/src/lib/Background/Background.tsx
+++ packages/components/src/lib/Background/Background.tsx
@@ -14,9 +14,25 @@ export const Background = ({ animate }: { animate?: boolean }) => {
       })}
       preserveAspectRatio="xMidYMin slice"
     >
-      <circle cx="1510" cy="358" r="290" stroke="var(--background-color)" />
+      <defs>
+        <linearGradient id="gradient-1" x1="0" y1="0" x2="90%" y2="80%">
+          <stop className={styles['Stop1']} offset="95%" stopOpacity={0} />
+          <stop className={styles['Stop2']} offset="100%" />
+        </linearGradient>
 
-      <circle cx={0} cy={337} r={650} stroke="var(--background-color)" />
+        <linearGradient id="gradient-2" x1="0" y1="100%" x2="0" y2="0">
+          <stop className={styles['Stop1']} offset="85%" stopOpacity={0} />
+          <stop className={styles['Stop2']} offset="100%" />
+        </linearGradient>
+
+        <linearGradient id="gradient-3" x1="0" y1="0" x2="0" y2="100%">
+          <stop className={styles['Stop1']} offset="75%" stopOpacity={0} />
+          <stop className={styles['Stop2']} offset="100%" />
+        </linearGradient>
+      </defs>
+      <circle cx="1510" cy="358" r="290" className={styles['Gradient-3']} />
+
+      <circle cx={0} cy={337} r={650} className={styles['Gradient-1']} />
       <circle
         cx={200}
         cy={337 + 650 + 50}
@@ -26,12 +42,24 @@ export const Background = ({ animate }: { animate?: boolean }) => {
         fill="var(--fill)"
       />
 
-      <circle cx="1321" cy="1349" r="628.5" stroke="var(--background-color)" />
+      <circle cx="1321" cy="1349" r="628.5" className={styles['Gradient-2']} />
 
-      <circle cx="1232" cy="-119" r="195.5" stroke="var(--background-color)" />
-      <circle cx="1054.5" cy="198.5" r="70" stroke="var(--background-color)" />
       <circle
-        className={styles['Planets1']}
+        className="dark:hidden"
+        cx="1232"
+        cy="-119"
+        r="195.5"
+        stroke="var(--background-color)"
+      />
+      <circle
+        className="dark:hidden"
+        cx="1054.5"
+        cy="198.5"
+        r="70"
+        stroke="var(--background-color)"
+      />
+      <circle
+        className={classNames(styles['Planets1'], 'dark:hidden')}
         transform-origin="1054.5px 198.5px"
         cx="952"
         cy="238"
@@ -39,7 +67,7 @@ export const Background = ({ animate }: { animate?: boolean }) => {
         fill="var(--fill)"
       />
       <circle
-        className={styles['Planets1']}
+        className={classNames(styles['Planets1'], 'dark:hidden')}
         transform-origin="1054.5px 198.5px"
         cx="1168"
         cy="202"
@@ -49,7 +77,13 @@ export const Background = ({ animate }: { animate?: boolean }) => {
       <circle cx="522" cy="1184" r="151.5" stroke="var(--background-color)" />
       <circle cx="-24.5" cy="1510.5" r="360" stroke="var(--background-color)" />
       <circle cx="648.5" cy="2490.5" r="672" stroke="var(--background-color)" />
-      <circle cx="1801" cy="-152" r="252.5" stroke="var(--background-color)" />
+      <circle
+        className="dark:hidden"
+        cx="1801"
+        cy="-152"
+        r="252.5"
+        stroke="var(--background-color)"
+      />
     </svg>
   )
 }
