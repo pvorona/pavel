4451885df491f50343bec5ce3b9f57e23776d33e
pavel <vorona.pavel.nickolaevich@gmail.com>
2022-04-20 17:54:04 +0300

    [Notifications] wrap function

--- packages/notification-service/src/app/notification.test.js
+++ packages/notification-service/src/app/notification.test.js
@@ -1,10 +1,12 @@
 import fetch from 'node-fetch'
 
-fetch(
-  'https://europe-central2-notification-service-347307.cloudfunctions.net/notification',
-  {
-    method: 'POST',
-    body: JSON.stringify({ message: 'Good night' }),
-    headers: { authorization: 'fb205aac-97db-1764-8359-4242cbb10c79' },
-  },
-).catch(console.error)
+export function sendNotification(message) {
+  return fetch(
+    'https://europe-central2-notification-service-347307.cloudfunctions.net/notification',
+    {
+      method: 'POST',
+      body: JSON.stringify({ message }),
+      headers: { authorization: 'fb205aac-97db-1764-8359-4242cbb10c79' },
+    },
+  )
+}
