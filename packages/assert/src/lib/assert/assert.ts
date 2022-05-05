6ec2006d93f02f8ebead66c34318a29fd7cc6e8a
pavel <vorona.pavel.nickolaevich@gmail.com>
2022-05-05 23:16:55 +0300

    [Assert] use asserts annotation

--- packages/assert/src/lib/assert/assert.ts
+++ packages/assert/src/lib/assert/assert.ts
@@ -1,6 +1,9 @@
 export const DEFAULT_MESSAGE = 'Assertion Error'
 
-export function assert(condition: boolean, message?: string): void {
+export function assert(
+  condition: boolean,
+  message?: string,
+): asserts condition {
   if (!condition) {
     throw new Error(message || DEFAULT_MESSAGE)
   }
