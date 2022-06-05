0ad842e5561da1cec4ab6f9c2d41edfbcce00373
pavel <vorona.pavel.nickolaevich@gmail.com>
2022-06-05 10:44:37 +0300

    [Types] Use `Symbol.species` in `Nominal`

--- packages/types/src/lib/index.ts
+++ packages/types/src/lib/index.ts
@@ -3,7 +3,7 @@ export type Lambda = () => void
 export type RecordKey = string | number | symbol
 
 export type Nominal<Source, Type extends string> = Source & {
-  __TYPE__: Type
+  [Symbol.species]: Type
 }
 
 export type AnyRecord = Record<RecordKey, unknown>
