e1a7be1b254713e92eeb1c109522159b903dcaa2
pavel <vorona.pavel.nickolaevich@gmail.com>
2023-05-19 19:22:15 +0300

    [Workspace] Update dependencies

--- packages/trading/jest.config.ts
+++ packages/trading/jest.config.ts
@@ -1,13 +1,15 @@
+/* eslint-disable */
 export default {
   displayName: 'trading',
   preset: '../../jest.preset.js',
-  globals: {
-    'ts-jest': {
-      tsconfig: '<rootDir>/tsconfig.spec.json',
-    },
-  },
+  globals: {},
   transform: {
-    '^.+\\.[tj]s$': 'ts-jest',
+    '^.+\\.[tj]s$': [
+      'ts-jest',
+      {
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+      },
+    ],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
   coverageDirectory: '../../coverage/packages/trading',
