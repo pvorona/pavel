ecfed9097d7e5f1745b778114a20effd15141d7d
pavel <vorona.pavel.nickolaevich@gmail.com>
2023-05-28 17:08:59 +0300

    Update dependencies

--- packages/chart-demo/jest.config.ts
+++ packages/chart-demo/jest.config.ts
@@ -1,9 +1,10 @@
+/* eslint-disable */
 export default {
   displayName: 'chart-demo',
 
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   transform: {
-    '^.+\\.[tj]s$': 'babel-jest',
+    '^.+\\.[tj]s$': ['babel-jest', { presets: ['@nx/react/babel'] }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
   coverageDirectory: '../../coverage/packages/chart-demo',
