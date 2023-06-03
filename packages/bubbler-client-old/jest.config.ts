07adf584a6675bea4f3068af3fdb7a6d33252664
pavel <vorona.pavel.nickolaevich@gmail.com>
2023-06-03 08:07:31 +0300

    Recreate client

--- packages/bubbler-client-old/jest.config.ts
+++ packages/bubbler-client-old/jest.config.ts
@@ -1,11 +1,11 @@
 /* eslint-disable */
 export default {
-  displayName: 'bubbler',
+  displayName: 'bubbler-client-old',
   preset: '../../jest.preset.js',
   transform: {
     '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
     '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/next/babel'] }],
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../coverage/packages/bubbler',
+  coverageDirectory: '../../coverage/packages/bubbler-client-old',
 }
