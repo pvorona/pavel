3b6f1d9fc4c1ac92ed60ab2cd0826af1766ec58e
pavel <vorona.pavel.nickolaevich@gmail.com>
2023-12-02 14:15:07 +0300

    Remove proxy related code

--- packages/observable/src/lib/internal/internal.ts
+++ packages/observable/src/lib/internal/internal.ts
@@ -1,30 +1,3 @@
 import { RecordKey } from '@pavel/types'
 
-const proxyToRawMap = new WeakMap()
-const rawToProxyMap = new WeakMap()
-
 export type Target = Record<RecordKey, unknown>
-
-export function isProxy(value: Target) {
-  return proxyToRawMap.has(value)
-}
-
-export function hasCorrespondingProxy(value: Target) {
-  return rawToProxyMap.has(value)
-}
-
-export function getProxyFor(value: Target) {
-  return rawToProxyMap.get(value)
-}
-
-export function getRawForProxy(proxy: Target) {
-  return proxyToRawMap.get(proxy)
-}
-
-export function setProxyForRaw(raw: Target, proxy: Target) {
-  rawToProxyMap.set(raw, proxy)
-}
-
-export function setRawForProxy(proxy: Target, raw: Target) {
-  proxyToRawMap.set(proxy, raw)
-}
